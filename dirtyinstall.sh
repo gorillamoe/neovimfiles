32b62af0db56d03c52cc8b7d6a425aaa85c47b95
Marco Kellershoff <marco.kellershoff@gmail.com>
2017-11-04 11:16:26 +0100

    Move to Vim 8+ native plugin management

--- dirtyinstall.sh
+++ dirtyinstall.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
-cd ~
-mkdir -p Code/walialu
-git clone https://github.com/walialu/vimfiles Code/walialu/vimfiles
-ln -s Code/walialu/vimfiles/.vim
-ln -s Code/walialu/vimfiles/.vimrc
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim "+PlugInstall" "+qall"
-# Hopefully this will fix the weird broken terminal
-printf "\ec" #\e is ESC in bash
-echo -en "\ec" #thanks @Jonathon Reinhart.
+#!/bin/bash
+
+main() {
+        repo_url="https://github.com/walialu/vimfiles"
+        user=$(whoami)
+        dir="$HOME/Code/$user/vimfiles"
+        packdir="$dir/.vim/pack/superevil"
+        mkdir -p "$dir"
+        git clone "$repo_url" "$dir"
+        ln -s "$dir/.vim" "$HOME/.vim"
+        ln -s "$dir/.vimrc" "$HOME/.vimrc"
+        mkdir -p "$packdir/start"
+        mkdir -p "$packdir/opt"
+}
