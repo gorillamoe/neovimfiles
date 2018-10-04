942f9f7720af8d5c6dd015188f0ad68f89e64a6b
Marco Kellershoff <hello@gorilla.moe>
2018-10-05 01:03:36 +0200

    Fix install script

--- install.bash
+++ install.bash
@@ -1,17 +1,12 @@
 #!/bin/bash
 
 setup() {
-        repo_url="git@git.superevilmegaco.com:marco/neovimfiles.git"
-        user=$(whoami)
-        vimdir=$(get_nvim_dir)
-        dir=$(get_self_repo_dir)
-        packdir="$vimdir/pack/superevil"
-        mkdir -p "$dir"
+        local repo_url="git@git.superevilmegaco.com:marco/neovimfiles.git"
+        local user=$(whoami)
+        local dir="$HOME/code/$user/neovimfiles"
         git clone "$repo_url" "$dir"
         ln -s "$dir/nvim" "$HOME/.config"
-        mkdir -p "$packdir/start"
-        mkdir -p "$packdir/opt"
-        nvim +PlugInstall
+        nvim +PlugInstall +UpdateRemotePlugins
 }
 
 setup
