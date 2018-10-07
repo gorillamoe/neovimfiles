3d195a47ea2337eedf89cd4ae218f19581e4bc8b
Marco Kellershoff <hello@gorilla.moe>
2018-10-07 13:19:37 +0200

    Make install script never fail
    Add checks for existing dirs (move them, before cloning and symlinking).

--- install.bash
+++ install.bash
@@ -1,11 +1,25 @@
 #!/bin/bash
 
 setup() {
-        local repo_url="git@git.superevilmegaco.com:marco/neovimfiles.git"
+        local repo_url="https://git.superevilmegaco.com/marco/neovimfiles"
         local user=$(whoami)
-        local dir="$HOME/code/$user/neovimfiles"
+        local homedir=$(getent passwd "$user" | cut -d: -f6)
+        local dir="$homedir/code/$user/neovimfiles"
+        local timestamp=$(date +%s)
+        if [[ -d "$dir" ]] || [[ -L "$dir" ]]; then
+                echo "Found existing cloned neovimfiles"
+                echo "Moving to backup location ..."
+                echo mv "$dir"{,.bak.$timestamp}
+                mv "$dir"{,.bak.$timestamp}
+        fi
+        if [[ -d "$homedir/.config/nvim" ]] || [[ -L "$homedir/.config/nvim" ]]; then
+                echo "Found existing config dir"
+                echo "Moving to backup location ..."
+                echo mv "$homedir/.config/nvim"{,.bak.$timestamp}
+                mv "$homedir/.config/nvim"{,.bak.$timestamp}
+        fi
         git clone "$repo_url" "$dir"
-        ln -s "$dir/nvim" "$HOME/.config"
+        ln -s "$dir/nvim" "$homedir/.config"
         nvim +PlugInstall +UpdateRemotePlugins
 }
 
