fa09517174ba1cd08f3f791bcfa349b02dc3634d
Marco Kellershoff <hello@gorilla.moe>
2019-01-01 19:50:31 +0100

    Fix install script
    Escape backticks in help docs

--- install.bash
+++ install.bash
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 __python_pip2_module_installed() {
-        local err=$(python2.7 -c "import $1")
+        local err
+        err=$(python2.7 -c "import $1")
         if [[ -z $err ]]; then
                 echo 1
         else
@@ -10,7 +11,8 @@ __python_pip2_module_installed() {
 }
 
 __python_pip_module_installed() {
-        local err=$(python -c "import $1")
+        local err
+        err=$(python -c "import $1")
         if [[ -z $err ]]; then
                 echo 1
         else
@@ -19,7 +21,8 @@ __python_pip_module_installed() {
 }
 
 __command_exists() {
-        local exists=$(command -v "$1")
+        local exists
+        exists=$(command -v "$1")
         # command does not exist
         if [[ -z $exists ]]; then
                 echo 0
@@ -39,17 +42,17 @@ __check_requirements() {
         fi
         if [[ $(__python_pip2_module_installed "neovim") == 0 ]]; then
                 echo "Neovim library for Python 2.7 required"
-                echo "Install (on Arch) with `yay -S python2-neovim`"
+                echo "Install (on Arch) with \`yay -S python2-neovim\`"
                 exit 3
         fi
         if [[ $(__python_pip_module_installed "neovim") == 0 ]]; then
                 echo "Neovim library for Python 3 required"
-                echo "Install (on Arch) with `yay -S python-neovim`"
+                echo "Install (on Arch) with \`yay -S python-neovim\`"
                 exit 4
         fi
         if [[ $(__command_exists "ruby") == 0 ]]; then
                 echo "Ruby is required"
-                echo "Install (on Arch) with `yay -S ruby ruby-neovim`"
+                echo "Install (on Arch) with \`yay -S ruby ruby-neovim\`"
                 exit 5
         fi
         if [[ $(__command_exists "npm") == 0 ]]; then
@@ -60,22 +63,26 @@ __check_requirements() {
 
 setup() {
         local repo_url="https://git.superevilmegaco.com/marco/neovimfiles"
-        local user=$(whoami)
-        local homedir=$(getent passwd "$user" | cut -d: -f6)
-        local dir="$homedir/code/$user/neovimfiles"
-        local timestamp=$(date +%s)
+        local user
+        local homedir
+        local dir
+        local timestamp
+        user=$(whoami)
+        homedir=$(getent passwd "$user" | cut -d: -f6)
+        dir="$homedir/code/$user/neovimfiles"
+        timestamp=$(date +%s)
         __check_requirements
         if [[ -d "$dir" ]] || [[ -L "$dir" ]]; then
                 echo "Found existing cloned neovimfiles"
                 echo "Moving to backup location ..."
-                echo mv "$dir"{,.bak.$timestamp}
-                mv "$dir"{,.bak.$timestamp}
+                echo mv "$dir"{,.bak."$timestamp"}
+                mv "$dir"{,.bak."$timestamp"}
         fi
         if [[ -d "$homedir/.config/nvim" ]] || [[ -L "$homedir/.config/nvim" ]]; then
                 echo "Found existing config dir"
                 echo "Moving to backup location ..."
-                echo mv "$homedir/.config/nvim"{,.bak.$timestamp}
-                mv "$homedir/.config/nvim"{,.bak.$timestamp}
+                echo mv "$homedir/.config/nvim"{,.bak."$timestamp"}
+                mv "$homedir/.config/nvim"{,.bak."$timestamp"}
         fi
         git clone "$repo_url" "$dir"
         ln -s "$dir/nvim" "$homedir/.config"
