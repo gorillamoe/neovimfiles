52f5850c4a7159854dcbb3ee7e4ebaeb52985339
Marco Kellershoff <marco@kellershoff.net>
2025-02-24 02:40:08 +0100

    feat(conform): add kulala to conform

--- nvim/lua/plugins/config/conform-nvim.lua
+++ nvim/lua/plugins/config/conform-nvim.lua
@@ -2,14 +2,22 @@ return {
   "stevearc/conform.nvim",
   config = function()
     require("conform").setup({
+      formatters = {
+        kulala = {
+          command = "kulala-fmt",
+          args = { "format", "$FILENAME" },
+          stdin = false,
+        },
+      },
       formatters_by_ft = {
+        http = { "kulala" },
+        go = { "goimports", "gofmt" },
+        javascript = { "eslint", "prettier" },
         lua = { "stylua" },
         python = { "autoflake", "black", "isort" },
-        javascript = { "eslint", "prettier" },
+        terraform = { "terraform_fmt" },
         typescript = { "eslint", "prettier" },
         typescriptreact = { "eslint", "prettier" },
-        go = { "goimports", "gofmt" },
-        terraform = { "terraform_fmt" },
       },
       format_on_save = {
         all = true,
