b1bfc84156f8ba83acc9537371876809af4092e6
Marco Kellershoff <marco@kellershoff.net>
2025-04-16 17:06:21 +0200

    feat(markdown): enable obsidian on all markdown files

--- nvim/lua/plugins/config/obsidian-nvim.lua
+++ nvim/lua/plugins/config/obsidian-nvim.lua
@@ -1,12 +1,14 @@
 return {
   "obsidian-nvim/obsidian.nvim",
   version = "*",
-  lazy = true,
-  ft = "markdown",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
   opts = {
+    completion = {
+      nvim_cmp = false,
+      blink = true,
+    },
     workspaces = {
       {
         name = "personal",
@@ -15,6 +17,20 @@ return {
       {
         name = "work",
         path = "~/projects/work/vault",
+      },
+      {
+        name = "no-vault",
+        path = function()
+          return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+        end,
+        overrides = {
+          notes_subdir = vim.NIL,
+          new_notes_location = "current_dir",
+          templates = {
+            folder = vim.NIL,
+          },
+          disable_frontmatter = true,
+        },
       },
     },
   },
