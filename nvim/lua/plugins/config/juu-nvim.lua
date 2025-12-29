7374cddebe1d2095e18022ed9b344bb8f21f00a7
Marco Kellershoff <marco@mistweaverco.com>
2025-12-29 15:00:00 +0100

    chore(juu): add types

--- nvim/lua/plugins/config/juu-nvim.lua
+++ nvim/lua/plugins/config/juu-nvim.lua
@@ -3,5 +3,6 @@ local get_dir_path_if_exists = require("helper").get_dir_path_if_exists
 return {
   "mistweaverco/juu.nvim",
   dir = get_dir_path_if_exists("/home/marco/projects/personal/juu.nvim"),
+  ---@type JuuUserConfig
   opts = {},
 }
