abf1faa46aa71073760ab16156bb8055a415e59b
Marco Kellershoff <marco@mistweaverco.com>
2026-01-05 13:10:06 +0100

    perf(indent): debounce + remove local dir from luarc

--- nvim/lua/config/indent.lua
+++ nvim/lua/config/indent.lua
@@ -1,3 +1,6 @@
+local DEBOUNCER = vim.uv.new_timer()
+local DEBOUNCE_MS = 30000 -- 30 seconds
+
 --- Function to check for mixed indentation in a buffer
 --- @param bufnr number Buffer number (0 for current buffer)
 --- @return boolean True if mixed indentation is found, false otherwise
@@ -6,7 +9,6 @@ local function has_mixed_indentation(bufnr)
   bufnr = bufnr or 0
   local has_tab_indent = false
   local has_space_indent = false
-
   for idx, line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)) do
     local indent = line:match("^(%s+)")
     if indent then
@@ -22,24 +24,32 @@ local function has_mixed_indentation(bufnr)
       return true, idx
     end
   end
-
   return false, nil
 end
 
 vim.api.nvim_create_autocmd({ "BufEnter", "TextChanged", "TextChangedI" }, {
   callback = function(args)
-    if has_mixed_indentation(args.buf) then
-      print("Mixed indentation detected in buffer " .. args.buf)
-      vim.opt_local.list = true
-      vim.opt_local.listchars:append({
-        tab = "»·",
-        lead = " ",
-      })
-      vim.notify("Mixed indentation detected at line " .. vim.fn.line("."), vim.log.levels.WARN)
-    else
-      vim.opt_local.list = false
-    end
+    DEBOUNCER:stop()
+
+    DEBOUNCER:start(
+      DEBOUNCE_MS,
+      0,
+      vim.schedule_wrap(function()
+        if not vim.api.nvim_buf_is_valid(args.buf) then
+          return
+        end
+
+        if has_mixed_indentation(args.buf) then
+          vim.opt_local.list = true
+          vim.opt_local.listchars:append({
+            tab = "»·",
+            lead = " ",
+          })
+          vim.notify("Mixed indentation detected", vim.log.levels.WARN)
+        else
+          vim.opt_local.list = false
+        end
+      end)
+    )
   end,
 })
-
-local foo = 2
