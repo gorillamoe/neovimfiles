97a63d625a29a52f8c9a37c27b8c7ac3d511b9df
Marco Kellershoff <marco@mistweaverco.com>
2026-01-08 04:25:49 +0100

    feat(snap-nvim): update config

--- nvim/lua/plugins/config/snap-nvim.lua
+++ nvim/lua/plugins/config/snap-nvim.lua
@@ -2,16 +2,21 @@ local get_dir_path_if_exists = require("helper").get_dir_path_if_exists
 
 return {
   "mistweaverco/snap.nvim",
-  version = "v1.0.0",
+  version = "v1.5.0",
   dir = get_dir_path_if_exists("/home/marco/projects/personal/snap.nvim"),
+  cmd = "Snap",
+  event = "VeryLazy",
   ---@type SnapUserConfig
   opts = {
     template = "linux",
-    debug = {
+    development_mode = {
       backend = "bun",
-      log_level = "debug",
     },
+    log_level = "debug",
     filename_pattern = "snap",
+    save_to_disk = {
+      image = false,
+    },
     font_settings = {
       size = 14,
       line_height = 1.0,
