004785e2550c67d9ef56582eef2b448d04f18787
Marco Kellershoff <marco@mistweaverco.com>
2026-04-09 14:52:40 +0200

    fix(tsgo): no exit early

--- nvim/lsp/tsgo.lua
+++ nvim/lsp/tsgo.lua
@@ -6,28 +6,10 @@ local root_files = {
   ".git",
 }
 
--- Find the tsgo binary
-local tsgo_bin = vim.fn.exepath("tsgo")
-
--- tsgo binary was not found, exit early
-if tsgo_bin == "" then
-  return
-end
-
 -- Find the root directory by searching for root files,
 -- stopping at the user's home directory
 local paths = vim.fs.find(root_files, { stop = vim.env.HOME })
 local root_dir = vim.fs.dirname(paths[1])
-
--- root directory was not found
-if root_dir == nil then
-  return
-end
-
-if not (vim.fn.filereadable(root_dir .. "/deno.json") == 1) then
-  -- If deno.json is not present, we don't want to use tsgo
-  return
-end
 
 ---@type vim.lsp.Config
 local M = {}
