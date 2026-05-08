fb321a9690b3d7a29647890af22cbe18253e9033
Marco Kellershoff <marco@mistweaverco.com>
2026-05-08 21:54:23 +0200

    chore(plugins): update plugins and remove nvim-treesitter

--- nvim/lua/plugins/config/render-markdown-nvim.lua
+++ nvim/lua/plugins/config/render-markdown-nvim.lua
@@ -1,6 +1,6 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {
