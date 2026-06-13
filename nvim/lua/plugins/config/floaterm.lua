fc93f6c9a557d2a987bf66c5fb3319f586535be8
Marco Kellershoff <marco@mistweaverco.com>
2026-06-14 01:51:37 +0200

    feat(floaterm): add keybind

--- nvim/lua/plugins/config/floaterm.lua
+++ nvim/lua/plugins/config/floaterm.lua
@@ -4,8 +4,13 @@ return {
   opts = {
     mappings = {
       term = function(buf)
-        vim.keymap.set({ "n", "t" }, "<leader>t", function()
-          require("floaterm").toggle()
+        vim.keymap.set({ "n", "t" }, "<Esc>", function()
+          -- if in insert mode, exit to normal mode first, then toggle the terminal
+          if vim.api.nvim_get_mode().mode == "t" then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", true)
+          else
+            require("floaterm").toggle()
+          end
         end, { buffer = buf })
       end,
     },
