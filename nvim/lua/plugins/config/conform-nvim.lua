94f97ac8b6c1d1de9492623a668eaa4ec49b6b83
Marco Kellershoff <marco@mistweaverco.com>
2026-06-15 21:48:30 +0200

    perf(conform): add exlude_if condition to prevent formatter battle

--- nvim/lua/plugins/config/conform-nvim.lua
+++ nvim/lua/plugins/config/conform-nvim.lua
@@ -1,14 +1,30 @@
+---@class FormatterDefinition
+---@field name string The name of the formatter
+---@field cmd string The command to check for the formatter's availability
+---@field excluded_if? string[] A list of formatter names that, if available, should exclude this formatter from being used
+
 --- A cache to store available formatters
---- @type table<string> The formatter name
+--- @type table<string, string>
 local cache_available_formatters = {}
 
+---A function to check if a formatter needs to be excluded based on the presence of other formatters
+---@param formatter FormatterDefinition The formatter definition to check for exclusion
+local needs_exclusion = function(formatter)
+  for _, excluded_name in pairs(formatter.excluded_if or {}) do
+    if vim.fn.executable(excluded_name) == 1 or cache_available_formatters[excluded_name] ~= nil then
+      return true
+    end
+  end
+  return false
+end
+
 --- A function to check for available formatters
---- @param formatters table A list of formatter definitions
---- @return table|nil A list of available formatter names or nil if none are available
+--- @param formatters FormatterDefinition[] formatters A list of formatter definitions to check for availability
+--- @return table|nil available_formatters A list of available formatter names or nil if none are available
 --- @usage
 --- local available = return_formatters_if_available({
----  { name = "prettier", cmd = "prettier" },
----  { name = "eslint", cmd = "eslint" },
+---  { name = "prettier", cmd = "prettier", excluded_if = { "oxfmt" } },
+---  { name = "eslint", cmd = "eslint", excluded_if = { "oxfmt" } },
 ---  { name = "nonexistent", cmd = "nonexistent-cmd" },
 ---  })
 local return_formatters_if_available = function(formatters)
@@ -16,15 +32,19 @@ local return_formatters_if_available = function(formatters)
   for _, formatter in pairs(formatters) do
     local name = formatter.name
     if cache_available_formatters[name] ~= nil then
-      table.insert(available_formatters, cache_available_formatters[name])
+      if not needs_exclusion(formatter) then
+        table.insert(available_formatters, cache_available_formatters[name])
+      end
     else
-      if vim.fn.executable(formatter.cmd) == 1 then
-        if name == "oxfmt" and vim.fn.executable("vp") == 1 then
-          table.insert(available_formatters, "vp")
-          cache_available_formatters[name] = "vp"
-        else
-          table.insert(available_formatters, name)
-          cache_available_formatters[name] = name
+      if not needs_exclusion(formatter) then
+        if vim.fn.executable(formatter.cmd) == 1 then
+          if name == "oxfmt" and vim.fn.executable("vp") == 1 then
+            table.insert(available_formatters, "vp")
+            cache_available_formatters[name] = "vp"
+          else
+            table.insert(available_formatters, name)
+            cache_available_formatters[name] = name
+          end
         end
       end
     end
@@ -52,10 +72,10 @@ return {
           { name = "gofmt", cmd = "gofmt" },
         }),
         javascript = return_formatters_if_available({
-          { name = "deno_fmt", cmd = "deno" },
-          { name = "eslint", cmd = "eslint" },
+          { name = "deno_fmt", cmd = "deno", excluded_if = { "oxfmt", "vp" } },
+          { name = "eslint", cmd = "eslint", excluded_if = { "oxfmt", "vp" } },
           { name = "oxfmt", cmd = "oxfmt" },
-          { name = "prettier", cmd = "prettier" },
+          { name = "prettier", cmd = "prettier", excluded_if = { "oxfmt", "vp" } },
         }),
         lua = return_formatters_if_available({
           { name = "stylua", cmd = "stylua" },
@@ -70,20 +90,20 @@ return {
         }),
         svelte = return_formatters_if_available({
           { name = "oxfmt", cmd = "oxfmt" },
-          { name = "eslint", cmd = "eslint" },
-          { name = "prettier", cmd = "prettier" },
+          { name = "eslint", cmd = "eslint", excluded_if = { "oxfmt", "vp" } },
+          { name = "prettier", cmd = "prettier", excluded_if = { "oxfmt", "vp" } },
         }),
         typescript = return_formatters_if_available({
-          { name = "deno_fmt", cmd = "deno" },
-          { name = "eslint", cmd = "eslint" },
+          { name = "deno_fmt", cmd = "deno", excluded_if = { "oxfmt", "vp" } },
+          { name = "eslint", cmd = "eslint", excluded_if = { "oxfmt", "vp" } },
           { name = "oxfmt", cmd = "oxfmt" },
-          { name = "prettier", cmd = "prettier" },
+          { name = "prettier", cmd = "prettier", excluded_if = { "oxfmt", "vp" } },
         }),
         typescriptreact = return_formatters_if_available({
-          { name = "deno_fmt", cmd = "deno" },
-          { name = "eslint", cmd = "eslint" },
+          { name = "deno_fmt", cmd = "deno", excluded_if = { "oxfmt", "vp" } },
+          { name = "eslint", cmd = "eslint", excluded_if = { "oxfmt", "vp" } },
           { name = "oxfmt", cmd = "oxfmt" },
-          { name = "prettier", cmd = "prettier" },
+          { name = "prettier", cmd = "prettier", excluded_if = { "oxfmt", "vp" } },
         }),
       },
       format_on_save = function()
