93a44afb4a66afc6c5924639fb5ab244bc12b8dc
Marco Kellershoff <marco@mistweaverco.com>
2026-07-08 15:49:38 +0200

    feat(diffconflicts): add keybinds

--- nvim/lua/plugins/config/diffconflicts-nvim.lua
+++ nvim/lua/plugins/config/diffconflicts-nvim.lua
@@ -3,5 +3,11 @@ local get_dir = require("helper").get_project_dir_path_if_exists
 return {
   "mistweaverco/diffconflicts.nvim",
   dir = get_dir("diffconflicts.nvim"),
-  opts = {},
+  opts = {
+    keymaps = {
+      next_diff = "<leader>dn",
+      prev_diff = "<leader>dp",
+      accept = "<leader>da",
+    },
+  },
 }
