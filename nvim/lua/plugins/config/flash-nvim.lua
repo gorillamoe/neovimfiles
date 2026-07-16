41f0ee2c3d19c0063bd5894828fa4cac0b96aaa7
Marco Kellershoff <marco@mistweaverco.com>
2026-07-16 09:37:59 +0200

    feat(flash): add remote ops

--- nvim/lua/plugins/config/flash-nvim.lua
+++ nvim/lua/plugins/config/flash-nvim.lua
@@ -1,5 +1,15 @@
 return {
   "folke/flash.nvim",
+  keys = {
+    {
+      "r", -- The GOAT - remote ops
+      mode = { "o" },
+      function()
+        require("flash").remote()
+      end,
+      desc = "Flash",
+    },
+  },
   config = function()
     require("flash").setup({
       modes = {
