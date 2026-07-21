7dbb22ce9c5ba3bf6b37df45dc6969ab9736a291
Marco Kellershoff <marco@mistweaverco.com>
2026-07-21 23:24:29 +0200

    feat(plugin): enable icons in fyler.nvim

--- nvim/lua/plugins/config/fyler-nvim.lua
+++ nvim/lua/plugins/config/fyler-nvim.lua
@@ -2,6 +2,9 @@ return {
   "FylerOrg/fyler.nvim",
   opts = {
     kind = "floating",
+    integrations = {
+      icon = 'nvim_web_devicons',
+    },
     ui = {
       hidden_items = {
         switches = {},
