fcbb883a8fde91989b87a561e90b0fcb5a8d2075
Marco Kellershoff <marco@mistweaverco.com>
2026-07-30 07:32:36 +0200

    fix(lualine): cleanup

--- nvim/lua/plugins/config/lualine-nvim.lua
+++ nvim/lua/plugins/config/lualine-nvim.lua
@@ -2,55 +2,48 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
     "kyazdani42/nvim-web-devicons",
+    "mistweaverco/jujutsu.nvim",
+    "mistweaverco/vhs-era-theme.nvim",
   },
-  config = function()
-    local icon = require("vhs-era-theme.icons")
-
-    local filetype = { "filetype", icon_only = true }
-
-    local diagnostics = {
-      "diagnostics",
-      sources = { "nvim_diagnostic" },
-      sections = { "error", "warn", "info", "hint" },
-      symbols = {
-        error = icon.diagnostics.BoldError .. " ",
-        hint = icon.diagnostics.BoldHint .. " ",
-        info = icon.diagnostics.BoldInformation .. " ",
-        warn = icon.diagnostics.BoldWarning .. " ",
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = "auto",
+      globalstatus = true,
+      component_separators = {
+        left = "",
+        right = "",
       },
-      colored = true,
-      update_in_insert = false,
-      always_visible = false,
-    }
-
-    require("lualine").setup({
-      options = {
-        icons_enabled = true,
-        theme = "auto",
-        globalstatus = true,
-        component_separators = {
-          left = "",
-          right = "",
+      section_separators = {
+        left = "",
+        right = "",
+      },
+      disabled_filetypes = { statusline = { "lazy" } },
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = {},
+      lualine_c = {
+        "floaterm",
+        "filename",
+        {
+          "diagnostics",
+          sources = { "nvim_diagnostic" },
+          sections = { "error", "warn", "info", "hint" },
+          colored = true,
+          update_in_insert = false,
+          always_visible = false,
         },
-        section_separators = {
-          left = "",
-          right = "",
-        },
-        disabled_filetypes = { statusline = { "lazy" } },
       },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = {},
-        lualine_c = { "floaterm", "filename", diagnostics },
-        lualine_x = require("jujutsu.lualine").prepend({
-          "kulala",
-          "encoding",
-          "fileformat",
-          filetype,
-        }),
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
+      lualine_x = {
+        "jujutsu",
+        "kulala",
+        "encoding",
+        "fileformat",
+        { "filetype", icon_only = true },
       },
-    })
-  end,
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
+  },
 }
