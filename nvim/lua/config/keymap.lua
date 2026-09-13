fea2d61b40a5c960e60b515fb358edc7a888925a
Marco Kellershoff <marco@the-dont-be-evil-company.com>
2026-09-13 20:31:27 +0200

    feat(plugins): replace oil with mini.files

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -98,7 +98,3 @@ end, {
 vim.keymap.set("i", "<C-e>", function()
   require("fzf-symbols").open()
 end, { desc = "Insert emoji or gitmoji", silent = true })
-
-map("n", "-", function()
-  require("oil").open_float()
-end, { desc = "Save file", silent = true })
---
+++ nvim/lua/plugins/config/mini-files.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-mini/mini.files",
+  opts = {},
+  keys = {
+    {
+      "-",
+      function()
+        local buf_dir = vim.fn.expand("%:p:h")
+        require("mini.files").open(buf_dir)
+      end,
+      desc = "Bafa",
+    },
+  },
+}
