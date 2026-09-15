d2db5fe780ee368b6374036bc045240a7e3da1c8
Marco Kellershoff <marco@the-dont-be-evil-company.com>
2026-09-15 11:45:05 +0200

    feat(mini-files): make nav like oil.nvim

--- nvim/lua/plugins/config/mini-files.lua
+++ nvim/lua/plugins/config/mini-files.lua
@@ -1,16 +1,63 @@
+--- INFO:
+--- This setup (the complete file)
+--- makes `mini.files` almost behave like `oil.nvim` 🥥.
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesBufferCreate",
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWriteCmd", {
+      buffer = args.data.buf_id,
+      callback = function()
+        require("mini.files").synchronize()
+      end,
+    })
+    vim.api.nvim_buf_set_keymap(
+      args.data.buf_id,
+      "n",
+      "<Esc>",
+      ":lua require('mini.files').close()<CR>",
+      { noremap = true, silent = true }
+    )
+  end,
+})
+
 return {
   "nvim-mini/mini.files",
   opts = {
     mappings = {
-      synchronize = "<CR>",
+      close = "q",
+      go_in = "L",
+      go_in_plus = "<CR>",
+      go_out = "-",
+      go_out_plus = "H",
+      reset = "<BS>",
+      reveal_cwd = "@",
+      show_help = "g?",
+      synchronize = "<leader>w",
+      trim_left = "<",
+      trim_right = ">",
+    },
+    options = {
+      permanent_delete = false,
+      use_as_default_explorer = true,
+    },
+    windows = {
+      preview = false,
+      width_focus = 30,
+      width_nofocus = 15,
     },
   },
   keys = {
     {
       "-",
       function()
-        local buf_dir = vim.fn.expand("%:p:h")
-        require("mini.files").open(buf_dir)
+        local path = vim.api.nvim_buf_get_name(0)
+        local MiniFiles = require("mini.files")
+        if path ~= "" and vim.uv.fs_stat(path) then
+          MiniFiles.open(path)
+        else
+          MiniFiles.open(vim.fn.getcwd())
+        end
       end,
       desc = "Bafa",
     },
